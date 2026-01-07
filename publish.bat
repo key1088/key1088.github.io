e391345304c90888d823dfc737aef3d811f06aba
key1088@gmail.com <key1088@gmail.com>
2026-01-07 11:43:14 +0800

    Remove .deploy_git from repo

---
+++ publish.bat
@@ -0,0 +1,11 @@
+@echo off
+echo ==============================
+echo Hexo Publish Start
+echo ==============================
+cd /d D:\project\key1088.github.io
+call hexo g
+call hexo d
+echo ==============================
+echo Hexo Publish End
+echo ==============================
+pause
--- source/_posts/hello-world.md
+++
@@ -1,4 +0,0 @@
----
-title: Hello Hexo
----
-Welcome to [Hexo](https://hexo.io/)! 
